bd908d558a983d3c7b84c0d93fbb4f1926d79ec9
yarrman2 <yarrrman@yandex.ru>
2024-03-15 11:02:16 +0300

    first step

---
+++ lib/simplest_form.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "simplest_form/version"
+
+module SimplestForm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/simplest_form/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SimplestForm
+  VERSION = "0.1.0"
+end
